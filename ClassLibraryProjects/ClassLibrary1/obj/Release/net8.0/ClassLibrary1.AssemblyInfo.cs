603ad7b987f6ba1696974229aca898e20080a376
Kathy <xu_kathy@outlook.com>
2024-04-15 22:29:51 -0400

    update files

--- ClassLibraryProjects/ClassLibrary1/obj/Release/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibraryProjects/ClassLibrary1/obj/Release/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07a3b0d5618891f1a872e5f368ba82e74f58582")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
